488a3a112ba9d6dfc6136bad9fe302fa89207acb
Alibek <alibeknurtuleu@gmail.com>
2022-09-06 10:08:27 +0600

    make token

--- create_bot.py
+++ create_bot.py
@@ -2,9 +2,10 @@ from aiogram import Bot
 import os
 from aiogram.dispatcher import Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import config
 
 storage = MemoryStorage()
 
-bot = Bot(token=os.getenv('TOKEN'))
+bot = Bot(token=config.BOT_TOKEN, parse_mode='HTML')
 dp = Dispatcher(bot, storage=storage)
 
