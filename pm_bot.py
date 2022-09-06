9cf0164fa29a5083a8cf63c74f07cd26a5a6e328
Alibek <alibeknurtuleu@gmail.com>
2022-09-06 23:31:41 +0600

    fdg

--- pm_bot.py
+++ pm_bot.py
@@ -4,11 +4,16 @@ from handlers import view, edit
 from data_base.sqlite_db import sql_start
 import schedule
 from make_request import do_req
+import time
 
 async def on_startup(_):
     print('Bot Online')
     sql_start()
-    schedule.every().wednesday.at("23:29").do(do_req())
+    schedule.every().tuesday.at("23:33").do(do_req())
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
 view.register_handlers_view(dp)
 edit.register_handlers_edit(dp)
