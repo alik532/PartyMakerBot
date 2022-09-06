fa53eb69c655d688358e7ddd9277bd046be9fd33
Alibek <alibeknurtuleu@gmail.com>
2022-09-06 09:55:18 +0600

    dfgdf

--- make_request.py
+++ make_request.py
@@ -1,6 +1,6 @@
 import requests
 
-endpoint  = "https://alarmerbot.ru/?key=ecefc4-59d973-347ce8&message=Hello"
+endpoint  = "https://alarmerbot.ru/?key=b26f6d-22e518-467f0c&message=youtube.com"
 
 def do_req():
     get_responce = requests.get(endpoint)
